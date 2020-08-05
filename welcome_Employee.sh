ebb6010270dc2469f143844b8449c3af31e96d26
Himanshu <yadavhimanshu63@gmail.com>
2020-08-05 11:50:26 +0530

    Attendance

--- welcome_Employee.sh
+++ welcome_Employee.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 
 echo "Welcome to Employee Wage Computation Program"
+
+isPresent=1
+randomCheck=$(( RANDOM%2 ))
+
+if (( $isPresent==$randomCheck ))
+then
+    echo "Employee is Present"
+else
+    echo "Employee is Absent"
+fi
