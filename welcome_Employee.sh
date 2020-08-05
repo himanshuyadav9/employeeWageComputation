6ef3a0a4d044b16cd061b94a172375eb7254f91e
Himanshu <yadavhimanshu63@gmail.com>
2020-08-05 12:01:03 +0530

    Employee Wage

--- welcome_Employee.sh
+++ welcome_Employee.sh
@@ -3,11 +3,17 @@
 echo "Welcome to Employee Wage Computation Program"
 
 isPresent=1
+ratePerHour=20
 randomCheck=$(( RANDOM%2 ))
-
+empWage=0
 if (( $isPresent==$randomCheck ))
 then
-    echo "Employee is Present"
+        empHours=8
+        empWage=$(($ratePerHour*$empHours))
+        echo "$empWage"
 else
-    echo "Employee is Absent"
+      empHours=0
+      empWage=$(($ratePerHour*$empHours))
+      echo "$empWage"
 fi
+
